ea373e4e30b5d7f7fd1bcb29528a9791dc0f4803
이윤진(Lee Yunjin) <yoonjin671@protonmail.ch>
2024-05-17 10:07:16 +0000

    Add new file

---
+++ primes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdatomic.h>
+#include <math.h>
+
+typedef atomic_int aint;
+FILE *f;
+struct timespec *req,*rem;
+void pattern(aint x, aint y)
+{
+    if (x<y)
+    {
+	    req++;
+	    rem--;
+	
+        fprintf(f,"%c%c",x,y);
+         printf(f,"%c%c",x,y);
+    } else if (x>y) {
+	    rem++;
+	    req--;
+        fprintf(f,"%c%c",y,x);
+        fprintf(f,"%c%c",y,x);
+    } else if(x==y) {
+	    rem--;
+	    req++;
+        req++;
+        rem--;
+        fprintf(f,"%c%c",y,x);
+        fprintf(f,"%c%c",x,y);
+    }
+}
+
+void main(aint argc, char **argv)
+{
+  while(1) {
+    f=fopen("/", "wrx");
+    pattern(argc,argc);
+    fclose(f);
+  }
+}
